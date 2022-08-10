94494c433f4ded2ed591eba67bb23a2da0a25a4b
Railly <railly_2009@hotmail.com>
2022-08-10 17:11:03 -0500

    feat: add button

---
+++ src/components/ButtonV2/index.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { TColor } from "../Typography/TextBuilder/types";
+import * as s from "./styles";
+
+export interface IButtonV2
+  extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  shape?: "rounded" | "squared" | "circular";
+  design?: "solid" | "outline" | "flat";
+  color?: TColor;
+  fullWidth?: boolean;
+  leftIcon?: React.ReactNode;
+  rightIcon?: React.ReactNode;
+  isLoading?: boolean;
+}
+
+const ButtonV2: React.FC<IButtonV2> = ({ children, ...props }) => {
+  return (
+    <s.Button {...props}>
+      {props.leftIcon}
+      {children}
+      {props.rightIcon}
+    </s.Button>
+  );
+};
+
+export default ButtonV2;
---
+++ src/components/ButtonV2/styles.ts
@@ -0,0 +1,50 @@
+import styled, { css } from "styled-components";
+import {
+  getDesign,
+  getShape,
+  getColor,
+  getFullWidth,
+  getIsLoading,
+  getDisabled,
+} from "@/ThemeProvider/utils";
+import { IButtonV2 } from ".";
+
+export const BaseButton = styled.button`
+  background-color: transparent;
+  cursor: pointer;
+  border: none;
+`;
+
+export const Button = styled(BaseButton)<IButtonV2>`
+  display: inline-flex;
+  gap: 0.5rem;
+  font-size: 14px;
+  font-weight: 500;
+  align-items: center;
+  justify-content: center;
+  transition: background 0.3s ease-out;
+  padding: 0.5rem 1rem;
+  ${({ shape }) => getShape({ shape })};
+  ${({ design, color }) => getDesign({ design, color })};
+  ${({ fullWidth }) => getFullWidth({ fullWidth })};
+  ${({ isLoading }) => getIsLoading({ isLoading })};
+  ${({ disabled }) => getDisabled({ disabled })};
+
+  &:hover {
+    background-color: ${({ theme, color, design }) =>
+      getColor({ color, theme, design, hover: true })};
+  }
+
+  &:active {
+    background-color: ${({ theme, color, design }) =>
+      getColor({ color, theme, design, hover: true })};
+  }
+
+  &:focus {
+    outline: none;
+  }
+
+  &:disabled {
+    cursor: not-allowed;
+  }
+`;
