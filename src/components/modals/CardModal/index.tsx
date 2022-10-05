3143db5c5c46add92ff1c453f8ed8dfb0b1a782f
Felix Castro <felixgonzalocastro@gmail.com>
2022-10-05 00:13:56 -0500

    feat: add CardModel component
    TASK: POS-11255
    HU: POS-11122

---
+++ src/components/modals/CardModal/index.tsx
@@ -0,0 +1,58 @@
+import React from "react";
+import IconClose from "@/icons/IconClose";
+import { ModalContainer } from "../ModalContainer";
+import { CloseIcon, StyledCardModal } from "./styles";
+import { ICardModal } from "./types";
+
+export const CardModal: React.FC<ICardModal> = ({
+  children,
+  onClose,
+  backdropOpacity,
+  padding,
+  borderRadius,
+  borderRadiusInDesktop,
+  maxHeight,
+  height,
+  maxWidth,
+  width,
+  zIndex,
+  overflow,
+  contentLocation = "center",
+  contentLocationInDesktop = "center",
+  closeIcon,
+  closeIconInDesktop,
+}) => {
+  return (
+    <ModalContainer
+      onClose={onClose}
+      backdropOpacity={backdropOpacity}
+      zIndex={zIndex}
+      contentLocation={contentLocation}
+      contentLocationInDesktop={contentLocationInDesktop}
+    >
+      <StyledCardModal
+        padding={padding}
+        borderRadius={borderRadius}
+        borderRadiusInDesktop={borderRadiusInDesktop}
+        maxHeight={maxHeight}
+        height={height}
+        maxWidth={maxWidth}
+        width={width}
+        overflow={overflow}
+        contentLocation={contentLocation}
+        contentLocationInDesktop={contentLocationInDesktop}
+      >
+        {(closeIcon || closeIconInDesktop) && (
+          <CloseIcon
+            onClick={onClose}
+            closeIconInDesktop={closeIconInDesktop}
+            closeIcon={closeIcon}
+          >
+            <IconClose />
+          </CloseIcon>
+        )}
+        {children}
+      </StyledCardModal>
+    </ModalContainer>
+  );
+};
---
+++ src/components/modals/CardModal/styles.ts
@@ -0,0 +1,147 @@
+import styled, { css } from "styled-components";
+
+interface CardModalType {
+  borderRadius?: string;
+  borderRadiusInDesktop?: string;
+  padding?: string;
+  maxHeight?: string;
+  height?: string;
+  maxWidth?: string;
+  width?: string;
+  overflow?: string;
+  contentLocation?: "bottom" | "top" | "left" | "right" | "center";
+  contentLocationInDesktop?: "bottom" | "top" | "left" | "right" | "center";
+}
+
+export const StyledCardModal = styled.div<CardModalType>`
+  background-color: white;
+  border-radius: ${(props) =>
+    props.borderRadius ? props.borderRadius : "15px"};
+  max-height: ${(props) => (props.maxHeight ? props.maxHeight : "90vh")};
+  height: ${(props) => (props.height ? props.height : "auto")};
+  max-width: ${(props) => (props.maxWidth ? props.maxWidth : "90vw")};
+  width: ${(props) => (props.width ? props.width : "90vw")};
+  padding: ${(props) => (props.padding ? props.padding : "1.5rem")};
+  overflow: ${(props) => props.overflow || "hidden"};
+  cursor: auto;
+  box-shadow: 0px 0px 24px 4px rgba(0, 12, 51, 0.05);
+  ${(props) => {
+    if (props.contentLocation && props.contentLocation === "bottom")
+      return css`
+        border-bottom-left-radius: 0;
+        border-bottom-right-radius: 0;
+        max-width: unset;
+        width: 100vw;
+      `;
+    if (props.contentLocation && props.contentLocation === "top")
+      return css`
+        border-top-left-radius: 0;
+        border-top-right-radius: 0;
+        max-width: unset;
+        width: 100vw;
+      `;
+    if (props.contentLocation && props.contentLocation === "left")
+      return css`
+        border-top-left-radius: 0;
+        border-bottom-left-radius: 0;
+        height: 100vh;
+        max-height: ${() => (props.maxHeight ? props.maxHeight : "100vh")};
+        max-width: ${() => (props.maxWidth ? props.maxWidth : "550px")};
+      `;
+    if (props.contentLocation && props.contentLocation === "right")
+      return css`
+        border-top-right-radius: 0;
+        border-bottom-right-radius: 0;
+        height: 100vh;
+        max-height: ${() => (props.maxHeight ? props.maxHeight : "100vh")};
+        max-width: ${() => (props.maxWidth ? props.maxWidth : "550px")};
+      `;
+  }}
+
+  ${(props) => {
+    if (props.contentLocationInDesktop === "center")
+      return css`
+        @media screen and (min-width: 1024px) {
+          border-radius: ${() =>
+            props.borderRadiusInDesktop ? props.borderRadiusInDesktop : "15px"};
+          height: auto;
+          max-width: ${() => (props.maxWidth ? props.maxWidth : "90vw")};
+          max-height: ${() => (props.maxHeight ? props.maxHeight : "90vh")};
+          padding: ${() => (props.padding ? props.padding : "1.5rem")};
+        }
+      `;
+    if (props.contentLocationInDesktop === "bottom")
+      return css`
+        @media screen and (min-width: 1024px) {
+          border-radius: ${() =>
+            props.borderRadiusInDesktop ? props.borderRadiusInDesktop : "15px"};
+          border-bottom-left-radius: 0;
+          border-bottom-right-radius: 0;
+          max-width: unset;
+          height: unset;
+          width: ${() => (props.width ? props.width : "100vw")};
+        }
+      `;
+    if (props.contentLocationInDesktop === "top")
+      return css`
+        @media screen and (min-width: 1024px) {
+          border-radius: ${() =>
+            props.borderRadiusInDesktop ? props.borderRadiusInDesktop : "15px"};
+          border-top-left-radius: 0;
+          border-top-right-radius: 0;
+          max-width: unset;
+          height: unset;
+          width: ${() => (props.width ? props.width : "100vw")};
+        }
+      `;
+    if (props.contentLocationInDesktop === "left")
+      return css`
+        @media screen and (min-width: 1024px) {
+          border-radius: ${() =>
+            props.borderRadiusInDesktop ? props.borderRadiusInDesktop : "15px"};
+          border-top-left-radius: 0;
+          border-bottom-left-radius: 0;
+          height: 100vh;
+          max-height: ${() => (props.maxHeight ? props.maxHeight : "100vh")};
+          max-width: ${() => (props.maxWidth ? props.maxWidth : "550px")};
+        }
+      `;
+    if (props.contentLocationInDesktop === "right")
+      return css`
+        @media screen and (min-width: 1024px) {
+          border-radius: ${() =>
+            props.borderRadiusInDesktop ? props.borderRadiusInDesktop : "15px"};
+          border-top-right-radius: 0;
+          border-bottom-right-radius: 0;
+          height: 100vh;
+          max-height: ${() => (props.maxHeight ? props.maxHeight : "100vh")};
+          max-width: ${() => (props.maxWidth ? props.maxWidth : "550px")};
+        }
+      `;
+  }}
+`;
+
+interface CloseIconProps {
+  closeIcon?: boolean;
+  closeIconInDesktop?: boolean;
+}
+
+export const CloseIcon = styled.div<CloseIconProps>`
+  background-color: white;
+  box-shadow: 0 2px 7px #e5e5e5;
+  border-radius: 50%;
+  width: 28px;
+  height: 28px;
+  display: ${({ closeIcon }) => (closeIcon ? "flex" : "none")};
+  justify-content: center;
+  align-items: center;
+  position: absolute;
+  top: 20px;
+  right: 20px;
+  font-size: 11px;
+  cursor: pointer;
+  @media screen and (min-width: 1024px) {
+    display: ${({ closeIconInDesktop }) =>
+      closeIconInDesktop ? "flex" : "none"};
+  }
+`;
