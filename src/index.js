9b8d91bcaec98767a4a407f054bb811bbed37800
Felix Castro <felixgonzalocastro@gmail.com>
2022-10-24 08:16:10 -0500

    refactor: move Card to cards folder

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ export { Text } from "@/components/Typography/Text";
 export { CustomSelect } from "@/components/CustomSelect";
 export { ProfileImage } from "@/components/ProfileImage";
 export { Loader } from "@/components/Loader";
-export { Card } from "@/components/Card";
+export { Card } from "@/components/cards/Card";
 export { Collapsible } from "@/components/Collapsible";
 export { StickyHeader } from "@/components/StickyHeader";
 export { DropdownMenu } from "@/components/dropdowns/DropdownMenu";
--- src/types.d.ts
+++ src/types.d.ts
@@ -6,7 +6,7 @@ export { Heading, Text } from "./components/Typography/TextBuilder/types";
 export { CustomSelect } from "./components/CustomSelect/types";
 export { ProfileImage } from "./components/ProfileImage/types";
 export { Loader } from "./components/Loader/types";
-export { Card } from "./components/Card/types";
+export { Card } from "./components/cards/Card/types";
 export { Collapsible } from "./components/Collapsible/types";
 export { StickyHeader } from "./components/StickyHeader/types";
 export { DropdownMenu } from "./components/dropdowns/DropdownMenu/types";
