05c7f256c647284fa506c360c6d7c8ab098ad2da
Diego Jimenez <dijim45@gmail.com>
2022-12-26 10:46:09 -0500

    feat: [POS-12852] add UserProfileIcon border

--- src/components/image-containers/UserProfileIcon/styles.ts
+++ src/components/image-containers/UserProfileIcon/styles.ts
@@ -1,8 +1,8 @@
 import styled from "styled-components";
 
 export const ImgContainer = styled.div<{ hasImg?: boolean }>`
-  background-color: ${({ hasImg, theme }) =>
-    hasImg ? theme.color.QUATERNARY[300] : "transparent"};
+  background-color: ${({ hasImg, theme }) => (hasImg ? theme.color.QUATERNARY[300] : "transparent")};
+  border: 2px solid white;
   border-radius: 50%;
   color: ${({ theme }) => theme.color.QUATERNARY[900]};
   display: flex;
@@ -10,7 +10,6 @@ export const ImgContainer = styled.div<{ hasImg?: boolean }>`
   align-items: center;
   overflow: hidden;
   font-weight: 500;
-  font-size: var(--test);
 
   img {
     width: 100%;
