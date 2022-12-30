f71601c30cc592727f23db0bb82c8a5f07cc6edb
Diego Jimenez <dijim45@gmail.com>
2022-12-30 15:20:26 -0500

    remove commented code

--- src/components/modals/ActionOptionsModal/docs/ActionOptionsModal.mock.tsx
+++ src/components/modals/ActionOptionsModal/docs/ActionOptionsModal.mock.tsx
@@ -36,32 +36,6 @@ const closeOnAction: IActionOptionsModal = {
   closeOnAction: true,
 };
 
-// const coloredOptions: IBulletOptionsModal = {
-//   title: "Cambiar estado",
-//   onClose: () => {},
-//   onSelect: (value, selected) => {},
-//   selected: "Aceptado",
-//   options: [
-//     { value: "Aceptado", content: "Aceptado", color: "#4318FF" },
-//     { value: "Preparando", content: "Preparando", color: "#F47B0A" },
-//     { value: "Preparado", content: "Preparado", color: "#FF82AF" },
-//     { value: "Enviado", content: "Enviado", color: "#5DDB92" },
-//   ],
-// };
-
-// const withDisabledOptions: IBulletOptionsModal = {
-//   title: "Cambiar estado",
-//   onClose: () => {},
-//   onSelect: (value, selected) => {},
-//   selected: "Aceptado",
-//   options: [
-//     { value: "Aceptado", content: "Aceptado", color: "#4318FF" },
-//     { value: "Preparando", content: "Preparando", color: "#F47B0A", disabled: true },
-//     { value: "Preparado", content: "Preparado", color: "#FF82AF", disabled: true },
-//     { value: "Enviado", content: "Enviado", color: "#5DDB92" },
-//   ],
-// };
-
 export const mock = {
   base,
   withIcons,
