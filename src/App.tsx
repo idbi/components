1be970128b60743c3408ab1302c946f1c4d06b3f
Felix Castro <felixgonzalocastro@gmail.com>
2023-01-10 15:30:44 -0500

    feat: add DateRangePicker test

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,43 @@
 import PlayGround from "./components/Playground";
+import { TextField } from "./components/TextField";
+import { DateRangePicker } from "./components/forms/DateRangePicker";
 
 function App() {
   return (
     <PlayGround>
       <p>test your components and clean when finished</p>
+      <DateRangePicker
+        onGetFilters={(data) => console.log("data", data)}
+      />
+      <DateRangePicker
+        initState={{
+          key: "felix",
+          endDate: new Date(Date.now()),
+          startDate: new Date(Date.now()),
+        }}
+        onGetFilters={(data) => console.log("data", data)}
+        dateFormatDisplayed="simple"
+        showIconCalendar
+
+      />
+      <DateRangePicker
+        onGetFilters={(data) => console.log("data", data)}
+        dateFormatDisplayed="simple"
+        showIconCalendar
+        showTimeInputs
+      />
+      <DateRangePicker
+        onGetFilters={(data) => console.log("data", data)}
+        showIconCalendar
+        initWithToday
+      />
+      <DateRangePicker
+        onGetFilters={(data) => console.log("data", data)}
+        showIconCalendar
+        initWithToday
+        showTimeInputs
+      />
+      <TextField />
     </PlayGround>
   );
 }
