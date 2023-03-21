f2859b08303ac3c39daa4a39a318ece87dada024
Felix Castro <felixgonzalocastro@gmail.com>
2023-03-21 11:45:03 -0500

    fix: reset storybook setup

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,23 +2,27 @@ const path = require("path");
 const { mergeConfig } = require("vite");
 
 module.exports = {
-  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
-  addons: [
+  "stories": [
+    "../src/**/*.stories.mdx",
+    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+  ],
+  "addons": [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
-    "@storybook/addon-docs",
+    "@storybook/addon-interactions"
   ],
-  framework: "@storybook/react",
-  core: {
-    builder: "@storybook/builder-vite",
+  "framework": "@storybook/react",
+  "core": {
+    "builder": "@storybook/builder-vite"
   },
-  features: {
-    storyStoreV7: true,
+  "features": {
+    "storyStoreV7": true
   },
   async viteFinal(config, { configType }) {
     return mergeConfig(config, {
-      resolve: { alias: { "@": path.resolve(__dirname, "../src") } },
+      resolve: {
+        alias: { '@': path.resolve(path.dirname(__dirname), "src")},
+      },
     });
   },
-};
+}
