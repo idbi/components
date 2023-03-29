d059b5218f7cfcdd5450791645e7a5b3c69fb86b
Felix Castro <felixgonzalocastro@gmail.com>
2023-03-29 09:21:08 -0500

    fix: improve code to copy in Collapsible

--- src/components/Collapsible/docs/Collapsible.stories.tsx
+++ src/components/Collapsible/docs/Collapsible.stories.tsx
@@ -1,3 +1,4 @@
+import dedent from "ts-dedent";
 import { ComponentStory, ComponentMeta } from "@storybook/react";
 import { Collapsible } from "./Container";
 import { ICollapsible } from "../types";
@@ -6,18 +7,45 @@ import { mockCollapsibleProps } from "./Collapsible.mock";
 export default {
   title: "components/Collapsible",
   component: Collapsible,
-  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
   argTypes: {},
 } as ComponentMeta<typeof Collapsible>;
 
-// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
 const Template: ComponentStory<typeof Collapsible> = (args) => (
   <Collapsible {...args} />
 );
 
 export const Base = Template.bind({});
-// More on args: https://storybook.js.org/docs/react/writing-stories/args
-
 Base.args = {
   ...mockCollapsibleProps.base,
 } as ICollapsible;
+Base.parameters = {
+  docs: {
+    source: {
+      code: dedent`
+      import { Button, Collapsible, IdThemeProvider } from '@idbi/components'
+      import { useState } from 'react'
+      
+      function App() {
+        const [show, setShow] = useState(false)
+      
+        return (
+          <IdThemeProvider>
+            <Collapsible
+              content={
+                <Button design='outline' leftIcon={<>🕵🏻</>}>
+                  Continuar
+                </Button>
+              }
+              label='Open Collapsible'
+              open={show}
+              setOpen={setShow}
+            />
+          </IdThemeProvider>
+        )
+      }
+      
+      export default App      
+      `,
+    },
+  },
+};
