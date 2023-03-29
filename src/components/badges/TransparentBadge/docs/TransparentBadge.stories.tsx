2215ee43cb6d11e46bf97922626d8d80550b4cdd
Felix Castro <felixgonzalocastro@gmail.com>
2023-03-29 17:23:26 -0500

    fix: improve code to copy in TransparentBadge

--- src/components/badges/TransparentBadge/docs/TransparentBadge.stories.tsx
+++ src/components/badges/TransparentBadge/docs/TransparentBadge.stories.tsx
@@ -1,3 +1,4 @@
+import dedent from "ts-dedent";
 import { ComponentStory, ComponentMeta } from "@storybook/react";
 import { Container } from "./Container";
 import { mockConfirmationCardV2Props } from "./TransparentBadge.mock";
@@ -13,4 +14,33 @@ const Template: ComponentStory<typeof Container> = () => <Container />;
 export const Base = Template.bind({});
 Base.args = {
   ...mockConfirmationCardV2Props.base,
+};
+Base.parameters = {
+  docs: {
+    source: {
+      code: dedent`
+      import {
+        IdThemeProvider,
+        TransparentBadge,
+      } from '@idbi/components'
+      
+      function App() {
+        return (
+          <IdThemeProvider>
+              <TransparentBadge intensity={0.3} opacity={0.1}>
+                1
+              </TransparentBadge>
+              <TransparentBadge intensity={0.5} opacity={0.2}>
+                Text
+              </TransparentBadge>
+          </IdThemeProvider>
+        )
+      }
+      
+      export default App      
+      `,
+      language: "tsx",
+      type: "auto",
+    },
+  },
 };
