68e945679e12131856b5a13a60d415dc57d33a7b
Felix Castro <felixgonzalocastro@gmail.com>
2023-03-29 17:24:44 -0500

    fix: improve code to copy in Radio

--- src/components/forms/Radio/docs/Radio.stories.tsx
+++ src/components/forms/Radio/docs/Radio.stories.tsx
@@ -1,3 +1,4 @@
+import dedent from "ts-dedent";
 import { ComponentStory, ComponentMeta } from "@storybook/react";
 import { Radio } from "./Container";
 import { IRadio } from "../types";
@@ -15,3 +16,31 @@ export const Base = Template.bind({});
 Base.args = {
   ...mockRadioProps.base,
 } as IRadio;
+Base.parameters = {
+  docs: {
+    source: {
+      code: dedent`
+      import {
+        IdThemeProvider,
+        Radio,
+      } from '@idbi/components'
+      import { useState } from 'react'
+      
+      function App() {
+        return (
+          <IdThemeProvider>
+            <Radio id='radioid' text='opción 1' name='radio' value='valor' />
+            <Radio id='radioid' text='opción 2' name='radio' value='valor' />
+            <Radio id='radioid' text='opción 3' name='radio' value='valor' />
+            <Radio id='radioid' text='opción 4' name='radio' value='valor' />
+          </IdThemeProvider>
+        )
+      }
+      
+      export default App          
+      `,
+      language: "tsx",
+      type: "auto",
+    },
+  },
+};
