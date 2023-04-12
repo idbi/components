875d1ccaf20d48a3af16fdfef85ae2bcd3640b39
Felix Castro <felixgonzalocastro@gmail.com>
2023-04-12 16:20:59 -0500

    style: fix eslint warnings

--- src/components/files-upload/UploadMultipleImages/index.tsx
+++ src/components/files-upload/UploadMultipleImages/index.tsx
@@ -1,26 +1,32 @@
 import React, { useEffect, useState } from "react";
-import useFirstRender from "@/hooks/useFirstRender";
-import { UploadFileContainer } from "../UploadFileContainer";
-
-import { IImgData, IUploadMultipleImages } from "./types";
 import { PictureIcon } from "@/icons-v2/PictureIcon";
 import { TrashIcon } from "@/icons-v2/TrashIcon";
+import useFirstRender from "@/hooks/useFirstRender";
+import { UploadFileContainer } from "../UploadFileContainer";
+import { IImgData, IUploadMultipleImages } from "./types";
 import * as s from "./styles";
 
-export const UploadMultipleImages = ({ onChange, initialImages, limit }: IUploadMultipleImages) => {
-  const [imgsList, setImgsList] = useState<(IImgData | string)[]>(initialImages || []);
+export const UploadMultipleImages = ({
+  onChange,
+  initialImages,
+  limit,
+}: IUploadMultipleImages) => {
+  const [imgsList, setImgsList] = useState<(IImgData | string)[]>(
+    initialImages || []
+  );
   const [showDeleteIndex, setShowDeleteIndex] = useState<number | null>(null);
   const firstRender = useFirstRender();
 
   const handleAddImage = (img: File) => {
     const imgUrl = window.URL.createObjectURL(img);
-    setImgsList((s) => [...s, { file: img, url: imgUrl }]);
+    setImgsList((i) => [...i, { file: img, url: imgUrl }]);
   };
 
   const handleDeleteImage = (index: number) => {
     const selectedImage = imgsList[index];
-    if (typeof selectedImage !== "string") window.URL.revokeObjectURL(selectedImage.url);
-    setImgsList((s) => s.filter((_, i) => i !== index));
+    if (typeof selectedImage !== "string")
+      window.URL.revokeObjectURL(selectedImage.url);
+    setImgsList((i) => i.filter((_, j) => j !== index));
   };
 
   useEffect(() => {
@@ -36,7 +42,7 @@ export const UploadMultipleImages = ({ onChange, initialImages, limit }: IUpload
           onMouseLeave={() => setShowDeleteIndex(null)}
           onTouchCancel={() => setShowDeleteIndex(null)}
         >
-          <img src={typeof img === "string" ? img : img.url} />
+          <img src={typeof img === "string" ? img : img.url} alt="" />
           {showDeleteIndex === i && (
             <s.Cover onClick={() => handleDeleteImage(i)}>
               <TrashIcon size={16} />
