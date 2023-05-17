f1c6f7384d80b9e36035494587ca32ff631b822d
Diego Jimenez <dijim45@gmail.com>
2023-05-17 16:58:00 -0500

    fix: fix removed props

--- src/components/cards/SectionedCard/index.tsx
+++ src/components/cards/SectionedCard/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import type { ISectionedCard } from "./types";
 import * as s from "./styles";
 
-export const SectionedCard = ({ header, body, footer, children }: ISectionedCard) => {
+export const SectionedCard = ({ children }: ISectionedCard) => {
   return <s.Card>{children}</s.Card>;
 };
 
