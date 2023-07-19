b2e5c8021ad147db4a782b64e9cd1864d321e1b3
Diego Jimenez <dijim45@gmail.com>
2023-07-19 14:37:23 -0500

    feat: [POS-16425] handle edit quantity input maximum value

--- src/components/forms/EditQuantityInput/styles.ts
+++ src/components/forms/EditQuantityInput/styles.ts
@@ -1,5 +1,5 @@
 /* eslint-disable no-nested-ternary */
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
 interface InputContainerProps {
   variant?: "card" | "base";
@@ -21,11 +21,7 @@ export const Container = styled.div`
 
 export const Span = styled.span<ISpanProps>`
   color: ${({ theme, isZero, error = false }) =>
-    isZero
-      ? error
-        ? theme.color.ALERT[900]
-        : theme.color.NEUTRAL[500]
-      : theme.color.PRIMARY[900]};
+    isZero ? (error ? theme.color.ALERT[900] : theme.color.NEUTRAL[500]) : theme.color.PRIMARY[900]};
   text-align: center;
   padding: 0 8px;
   min-width: 6ch;
@@ -49,8 +45,7 @@ export const InputContainer = styled.div<InputContainerProps>`
   transition: 0.3s ease-out;
   width: fit-content;
   align-self: flex-end;
-  border: ${({ theme, error = false }) =>
-    error ? `1px solid ${theme.color.ALERT[900]}` : "1px solid transparent"};
+  border: ${({ theme, error = false }) => (error ? `1px solid ${theme.color.ALERT[900]}` : "1px solid transparent")};
 
   @media (hover: hover) {
     :hover {
@@ -62,54 +57,52 @@ export const InputContainer = styled.div<InputContainerProps>`
     }
   }
 
-  button {
-    background-color: ${({ theme }) => theme.color.SECONDARY[900]};
-    border: none;
-    color: white;
-    border-radius: 50%;
-    width: ${({ variant }) => (variant === "card" ? "42px" : "35px")};
-    height: ${({ variant }) => (variant === "card" ? "40px" : "35px")};
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    transition: 0.3s ease-out;
-    font-size: ${({ variant }) => (variant === "card" ? "18px" : "16px")};
-
-    @media (hover: hover) {
-      :hover {
-        cursor: ${(props) => (props.isDisabled ? "auto" : "pointer")};
-        background-color: ${({ theme }) => theme.color.SECONDARY[700]};
-        transition: 0.3s ease-in;
-      }
-    }
-
-    :first-child {
-      background-color: ${(props) =>
-        props.minQuantity ? "white" : props.theme.color.SECONDARY[900]};
-      border: ${(props) =>
-        `1px solid ${
-          props.minQuantity
-            ? props.canDelete
-              ? props.theme.color.ALERT[900]
-              : props.theme.color.NEUTRAL[400]
-            : "none"
-        }`};
-      color: ${(props) =>
-        props.minQuantity
-          ? props.canDelete
-            ? props.theme.color.ALERT[900]
-            : props.theme.color.NEUTRAL[400]
-          : "white"};
-
-      :hover {
-        background-color: ${(props) =>
-          props.minQuantity ? "initial" : props.theme.color.SECONDARY[700]};
-      }
-    }
-  }
-
   @media (max-width: 850px) {
     background-color: white;
     box-shadow: 0px 1px 6px rgba(60, 60, 60, 0.2);
   }
+`;
+
+export const ActionButton = styled.button<{ alert?: boolean; card?: boolean }>`
+  background-color: ${({ theme }) => theme.color.SECONDARY[900]};
+  border: none;
+  color: white;
+  border-radius: 50%;
+  width: 35px;
+  height: 35px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  transition: 0.3s ease-out;
+  font-size: 16px;
+
+  @media (hover: hover) {
+    :hover:not(:disabled) {
+      cursor: pointer;
+      background-color: ${({ theme, alert }) => (alert ? "initial" : theme.color.SECONDARY[700])};
+      transition: 0.3s ease-in;
+    }
+  }
+
+  :disabled {
+    background-color: white;
+    border: 1px solid ${({ theme }) => theme.color.NEUTRAL[400]};
+    color: ${({ theme }) => theme.color.NEUTRAL[400]};
+  }
+
+  ${({ alert }) =>
+    alert &&
+    css`
+      background-color: white;
+      border: 1px solid ${({ theme }) => theme.color.ALERT[900]};
+      color: ${({ theme }) => theme.color.ALERT[900]};
+    `}
+
+  ${({ card }) =>
+    card &&
+    css`
+      width: 42px;
+      height: 40px;
+      font-size: 18px;
+    `}
 `;
