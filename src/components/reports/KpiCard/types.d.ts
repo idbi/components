05be98ee910eb68a48251cd5e6b605a0afc7805c
SamuelRnn <ricardo.reinn@outlook.com>
2023-11-06 13:18:19 -0500

    change tab to spaces on indent

--- src/components/reports/KpiCard/types.d.ts
+++ src/components/reports/KpiCard/types.d.ts
@@ -1,19 +1,19 @@
 import React, { ReactNode } from "react";
 
 export interface IKpiCard {
-	icon?: ReactNode;
-	title?: ReactNode;
-	total_PEN?: number;
-	total_USD?: number;
-	tip_PEN?: number;
-	tip_USD?: number;
-	main?: boolean;
-	autoWidth?: boolean;
-	loading?: boolean;
-	noDecimals?: boolean;
-	containerStyle?: CSSProperties;
-	quantity?: { title: string; value: number };
-	big?: boolean;
+  icon?: ReactNode;
+  title?: ReactNode;
+  total_PEN?: number;
+  total_USD?: number;
+  tip_PEN?: number;
+  tip_USD?: number;
+  main?: boolean;
+  autoWidth?: boolean;
+  loading?: boolean;
+  noDecimals?: boolean;
+  containerStyle?: CSSProperties;
+  quantity?: { title: string; value: number };
+  big?: boolean;
 }
 
 export declare const KpiCard: (props: IKpiCard) => JSX.Element;
